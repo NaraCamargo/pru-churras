7b8a7c5f60fb876baebb69a34d2478282f90186b
NaraCamargo <naraliggia2@gmail.com>
2023-03-22 08:34:15 -0300

    22/03

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,70 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  valendo = 1;
+  conta1 = 0;
+  conta2 = 0;
+  pontosTime1 = 0;
+  pontosTime2 = 0;
   constructor() {}
+
+  botao1(){
+    this.valendo = 1;
+  }
+
+  botao3(){
+    this.valendo = 3;
+  }
+
+  botao6(){
+    this.valendo = 6;
+  }
+
+  botao9(){
+    this.valendo = 9;
+  }
+
+  botao12(){
+    this.valendo = 12;
+  }
+
+
+  somaTime1(){
+    this.conta1 = this.conta1 + this.valendo;
+
+    if(this.conta1 >= 12){
+      this.conta1 = 0;
+      this.pontosTime1 = this.pontosTime1 + 1;
+    }
+  }
+
+  menosTime1(){
+    this.conta1 = this.conta1 - this.valendo;
+    if(this.conta1 < 0){
+      this.conta1 = 0;
+    }
+  }
+
+
+  somaTime2(){
+    this.conta2 = this.conta2 + this.valendo;
+    if(this.conta2 >= 12){
+      this.conta2 = 0;
+      this.pontosTime2 = this.pontosTime2 + 1;
+    }
+  }
+  menosTime2(){
+    this.conta2 = this.conta2 - this.valendo;
+    if(this.conta2 < 0){
+      this.conta2 = 0;
+
+    }
+  }
+  limpar(){
+    this.pontosTime1 = 0;
+    this.pontosTime2 = 0;
+
+  }
+
 
 }
